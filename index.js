412484957b6553e7b22bdac365bf6d1e0ca41705
steeks <andrei792@mail.ru>
2020-12-27 22:34:35 +0300

    Add scripts

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use("/triangle", require("./routes/triangle"));
 const port = 9000;
+
+if (process.env.NODE_ENV === "prodaction") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
